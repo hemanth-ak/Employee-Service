cbbccfaeb1504a89f130aa27892dd2c89a4eff7e
hemanth-ak <100900589+hemanth-ak@users.noreply.github.com>
2024-02-23 16:52:47 +0530

    Update Employee.java
    Moved the validations to dto class

--- employee/src/main/java/com/immaginnovate/employee/model/Employee.java
+++ employee/src/main/java/com/immaginnovate/employee/model/Employee.java
@@ -3,16 +3,12 @@ package com.immaginnovate.employee.model;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,21 +27,10 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long employeeId;
-    
-	@NotBlank(message = "The first name is required.")
 	private String firstName;
-	
-	@NotBlank(message = "The last name is required.")
 	private String lastName;
-	
-	@Email(message = "The email address is invalid.")
-	@NotBlank(message = "The email ID is required.")
 	private String emailId;
-	
 	private List<String> phoneNumbers;
-	
-	@JsonFormat(pattern="yyyy-MM-dd")
 	private LocalDate dateOfJoining;
-	
 	private int salary;
 }
